11d156f200191bed42121201c69dcd45d8626663
suhong <67825309+tnghd5761@users.noreply.github.com>
2021-08-23 23:55:12 +0900

    learn: register using Postman

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://suhong:jang8024@@@boilerplate.bffwg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
@@ -12,9 +18,21 @@ mongoose.connect('mongodb+srv://suhong:jang8024@@@boilerplate.bffwg.mongodb.net/
 })
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+	res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+
+	const user = new User(req.body)
+
+	user.save((err, userInfo) => {
+		if(err) return res.json({ success: false, err})
+		return res.status(200).json({
+			success: true
+		})
+	})
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+	console.log(`Example app listening at http://localhost:${port}`)
 })
