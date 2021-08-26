133b68c4cfc557d9e15e69444cf4c545363bc193
suhong <67825309+tnghd5761@users.noreply.github.com>
2021-08-26 22:37:15 +0900

    learn: encrypt password and login with jwt

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
-
+const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 
 const { User } = require("./models/User");
@@ -10,6 +10,7 @@ const { User } = require("./models/User");
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
@@ -24,7 +25,7 @@ app.get('/', (req, res) => {
 	res.send('Hello World! 안녕하세요!')
 })
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
 
 	const user = new User(req.body)
 
@@ -32,6 +33,33 @@ app.post('/register', (req, res) => {
 		if(err) return res.json({ success: false, err})
 		return res.status(200).json({
 			success: true
+		})
+	})
+})
+
+app.post('/api/users/login', (req, res) => {
+	// 요청된 이메일을 데이터베이스에서 있는지 탐색
+	User.findOne({ email: req.body.email }, (err, user) => {
+		if(!user) {
+			return res.json({
+				loginSuccess: false,
+				message: "제공된 이메일에 해당하는 유저가 없습니다."
+			})
+		}
+		// 요청된 이메일이 데이터베이스에서 있다면 비밀번호가 맞는지 확인
+		user.comparePassword(req.body.password, (err, isMatch) => {
+
+			if(!isMatch)
+				return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
+			// 비밀번호까지 맞다면 토큰 생성
+			user.generateToken((err, user) => {
+				if(err) return res.status(400).send(err);
+				
+				// 토큰 저장
+				res.cookie("x_auth", user.token)
+					.status(200)
+					.json({ loginSuccess: true, userId: user._id })
+			})
 		})
 	})
 })
